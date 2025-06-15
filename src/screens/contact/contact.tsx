bccd4c847fefc370253da0baa447929c87e1af9e
Mohammed Fadl <mohammadefathl@gmail.com>
2025-06-15 23:06:41 +0300

    refactor: compelete contact design , fix responsive in mobile

--- src/screens/contact/contact.tsx
+++ src/screens/contact/contact.tsx
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import './contact.css';
 import { TypingAnimation } from '../../components/typing-text/typing-text';
 import { useFirstVisit } from '../../hooks/useFirstVisit';
+import { useScreen } from '../../context/ScreenContext';
+import { Link } from 'react-router-dom';
 
 export const Contact: React.FC = () => {
   const isFirstVisit = useFirstVisit('contact');
+  const { isScreenOn } = useScreen();
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -21,32 +25,41 @@ export const Contact: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle form submission here
     console.log('Form submitted:', formData);
   };
 
   const personalInfo = `> Name: Mohammed Fathl Mohammed Al-Ghorbany
 > Phone: +967 778 940 511
 > Email: mohammedfadl.work@gmail.com
-> Birth Date: 2001/01/05`;
+> Birth Date: 2001/01/05 
+> Github: https://github.com/MoAlghorbani`;
 
-  return (
-    <div className='contact-container'>
-      <div className='contact-personal'>
-        <h2 className='text-q contact-title'>Personal Information</h2>
+  return <> 
+    {isScreenOn ? (
+      <div className='contact-container'>
+      <Link to={'/'} className='link'>{'<'} - RETURN TO MENU</Link>
+      <div className='contact-section contact-details-section'>
+        <h3 className='text-q contact-title'>CONTACT DETAILS</h3>
         <div className='personal-info'>
           <TypingAnimation text={personalInfo} duration={30} animate={isFirstVisit} />
         </div>
       </div>
-      <div className='contact-contact'>
-        <h2 className='text-q contact-title'>Contact Information</h2>
+      <div className='contact-section availability-section'>
+        <h3 className='text-q contact-title'>AVAILABILITY</h3>
+        <div className='availability-info'>
+          <TypingAnimation text={`> front-end development \n> Odoo development \n> Mobile development`} duration={30} animate={isFirstVisit} />
+        </div>
+      </div>
+      <div className='contact-section contact-form-section'>
+        <h3 className='text-q contact-title'>CONTACT FORM</h3>
         <form onSubmit={handleSubmit} className='contact-form'>
           <div className='form-group'>
-            <label style={{ display: 'flex', gap: '4px' }} htmlFor='name' className={`form-label ${formData.name ? 'filled' : 'empty'}`}>
+            <label htmlFor='name' style={{ display: 'flex', gap: '4px' }} className={`form-label ${formData.name ? 'filled' : 'empty'}`}>
               <span>Name</span><span>{'>> '}</span>
             </label>
             <input
               type='text'
+              id='name'
               name='name'
               value={formData.name}
               onChange={handleInputChange}
@@ -56,11 +69,12 @@ export const Contact: React.FC = () => {
             />
           </div>
           <div className='form-group'>
-            <label style={{ display: 'flex', gap: '4px' }} htmlFor='email' className={`form-label ${formData.email ? 'filled' : 'empty'}`}>
+            <label htmlFor='email' style={{ display: 'flex', gap: '4px' }} className={`form-label ${formData.email ? 'filled' : 'empty'}`}>
               <span>Email</span><span>{'>> '}</span>
             </label>
             <input
               type='email'
+              id='email'
               name='email'
               value={formData.email}
               onChange={handleInputChange}
@@ -70,10 +84,11 @@ export const Contact: React.FC = () => {
             />
           </div>
           <div className='form-group'>
-            <label style={{ display: 'flex', gap: '4px' }} htmlFor='message' className={`form-label ${formData.message ? 'filled' : 'empty'}`}>
+            <label htmlFor='message' style={{ display: 'flex', gap: '4px' }} className={`form-label ${formData.message ? 'filled' : 'empty'}`}>
               <span>Message</span><span>{'>> '}</span>
             </label>
             <textarea
+              id='message'
               name='message'
               value={formData.message}
               onChange={handleInputChange}
@@ -89,5 +104,6 @@ export const Contact: React.FC = () => {
         </form>
       </div>
     </div>
-  );
+    ) : null}
+  </>
 };
